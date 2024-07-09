172d4790dd4d8d194f9b3c87b8e6bb103b6579f4
Lazar Nikolov <hello@nikolovlazar.com>
2024-07-09 17:21:16 -0400

    injecting auth service in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,17 @@ import { type NextRequest, NextResponse } from 'next/server'
 
 import { updateSession } from '@/infrastructure/utils/supabase/middleware'
 
+import { IAuthenticationService } from './application/services/authentication-service.interface'
+import { inject } from './di/container'
+import { DI_TYPES } from './di/types'
+
 const protectedRoutes: string[] = ['/reset-password', '/app']
 export async function middleware(request: NextRequest) {
+  const authenticationService = inject<IAuthenticationService>(
+    DI_TYPES.AuthenticationService
+  )
+
+  console.log('Got the service!', authenticationService)
   let { user, response } = await updateSession(request)
 
   if (
