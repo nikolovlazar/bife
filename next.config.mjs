21a36ed42098bb193c6386630285aa55fc2245cf
Lazar Nikolov <hello@nikolovlazar.com>
2025-03-22 07:21:50 -0400

    changing sentry org

--- next.config.mjs
+++ next.config.mjs
@@ -30,7 +30,7 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options
 
-  org: 'sentry-devrel',
+  org: 'nikolovlazar',
   project: 'bife',
 
   // Only print logs for uploading source maps in CI
